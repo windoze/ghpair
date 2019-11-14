393d27704110bbd356ff91acc99a1e77fdb154b8
haha1903 <haha1903@gmail.com>
2019-11-14 15:34:43 +0800

    upgrade to new azure client and fix the chnroute script

--- install.sh
+++ install.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-AZURE_COM_ACCOUNT="$1"
-AZURE_CN_ACCOUNT="$2"
+AZURE_COM_SUB="$1"
+AZURE_CN_SUB="$2"
 
 cd `dirname $0`
 
 # Make tr work under OSX
 export LC_ALL=C
-GROUP_NAME="gohop3"
+GROUP_NAME="hai"
 
 # Generate passwords
-GHPASS=`tr -dc A-Za-z0-9 < /dev/urandom | fold -w 16 | head -n 1`
-SSPASS=`tr -dc A-Za-z0-9 < /dev/urandom | fold -w 16 | head -n 1`
+GHPASS=`date |md5 | head -c8`
+SSPASS=`date |md5 | head -c8`
 
 # Generate ssh key
 if [ ! -f $HOME/.ssh/id_rsa-gohop.pub ]; then
@@ -41,12 +41,12 @@ cat << EOF > server.param.json
 EOF
 
 echo "Logging into azure.com..."
-azure account set "$AZURE_COM_ACCOUNT"
+az cloud set -n AzureCloud
+az account set -s "$AZURE_COM_SUB"
 echo "Deploying Gohop server..."
-azure config mode arm
-azure group create $GROUP_NAME eastasia
-azure group deployment create $GROUP_NAME gohop-dep -f server/server.json -e server.param.json -v
-GHSADDR=`azure network public-ip list $GROUP_NAME --json | grep "ipAddress" | cut -d'"' -f4`
+az group create -n $GROUP_NAME -l southeastasia
+az group deployment create -g $GROUP_NAME --template-file server/server.json --parameters @server.param.json
+GHSADDR=`az network public-ip list -g $GROUP_NAME | grep "ipAddress" | cut -d'"' -f4`
 echo "Gohop server is running at $GHSADDR:40000-41000"
 
 # Generate gohop client parameters
@@ -75,19 +75,19 @@ cat << EOF > client.param.json
 EOF
 
 echo "Logging into azure.cn..."
-azure account set "$AZURE_CN_ACCOUNT"
+az cloud set -n AzureChinaCloud
+az account set -s "$AZURE_CN_SUB"
 echo "Deploying Gohop client and shadowsocks server..."
-azure config mode arm
-azure group create $GROUP_NAME chinaeast
-azure group deployment create $GROUP_NAME gohop-dep -f client/client.json -e client.param.json -v
-GHCADDR=`azure network public-ip list $GROUP_NAME --json | grep "ipAddress" | cut -d'"' -f4`
-echo "Shadowsocks server is running at $GHCADDR:8388"
+az group create -n $GROUP_NAME -l chinaeast2
+az group deployment create -g $GROUP_NAME --template-file client/client.json --parameters @client.param.json
+GHCADDR=`az network public-ip list -g $GROUP_NAME | grep "ipAddress" | cut -d'"' -f4`
+echo "Shadowsocks server is running at $GHCADDR:9488"
 
 echo "Using following config file for shadowsocks client:"
 cat << EOF
 {
     "server":"$GHCADDR",
-    "server_port":8388,
+    "server_port":9488,
     "local_address":"local_address_to_bind",
     "local_port": local_port_to_bind,
     "password":"$SSPASS",
@@ -95,7 +95,7 @@ cat << EOF
     "method":"aes-256-cfb"
 }
 EOF
-SSURL=`echo "aes-256-cfb:$SSPASS@$GHCADDR:8388" | base64`
+SSURL=`echo "aes-256-cfb:$SSPASS@$GHCADDR:9488" | base64`
 
 echo
 echo
